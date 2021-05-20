8a7ed139ea95469bfbcee90dae2950a383bbefda
AL3X09 <alexcisas@gmail.com>
2021-05-19 19:02:31 -0500

    19-05-2021

--- frontend/src/components/profile/Profile.component.js
+++ frontend/src/components/profile/Profile.component.js
@@ -3,7 +3,8 @@ import AuthService from "../../services/auth.service";
 
 import Header from '../header/Header.component';
 
-export default class Profile extends Component {
+class Profile extends Component {
+
     constructor(props) {
         super(props);
 
@@ -14,46 +15,50 @@ export default class Profile extends Component {
 
     render() {
         //console.log('currentUser');
-        
+
         const { currentUser } = this.state;
-        //<Header />
+        //const { header } = <Header />;
         return (
-            <Header />,
-            <div className="container">
-                <header className="jumbotron">
-                <h3><strong>{currentUser.user.username}</strong> Perfil</h3>
-                </header>
-                <p>
-                    <strong>Token:</strong>{" "}
-                    {currentUser.token.substring(0, 20)} ...{" "}
-                    {currentUser.token.substr(currentUser.token.length - 20)}
-                </p>
-                <p>
-                    <strong>Email:</strong>{" "}
-                    {currentUser.email}
-                </p>
-                <p>
-                    <strong>Id:</strong>{" "}
-                    {currentUser.user.id}
-                </p>
-                <p>
-                    <strong>name:</strong>{" "}
-                    {currentUser.user.name}
-                </p>
-                <p>
-                    <strong>last_name:</strong>{" "}
-                    {currentUser.user.last_name}
-                </p>
-                <p>
-                    <strong>Email:</strong>{" "}
-                    {currentUser.user.email}
-                </p>
-                <strong>Authorities:</strong>
-                <ul>
-                    {currentUser.roles &&
-                        currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
-                </ul>
-            </div>
+            <React.Fragment>
+                <Header />
+                <div className="">
+                    <header className="">
+                        <h3><strong>{currentUser.user.username}</strong> Perfil</h3>
+                    </header>
+                    <p>
+                        <strong>Token:</strong>{" "}
+                        {currentUser.token.substring(0, 20)} ...{" "}
+                        {currentUser.token.substr(currentUser.token.length - 20)}
+                    </p>
+                    <p>
+                        <strong>Email:</strong>{" "}
+                        {currentUser.email}
+                    </p>
+                    <p>
+                        <strong>Id:</strong>{" "}
+                        {currentUser.user.id}
+                    </p>
+                    <p>
+                        <strong>name:</strong>{" "}
+                        {currentUser.user.name}
+                    </p>
+                    <p>
+                        <strong>last_name:</strong>{" "}
+                        {currentUser.user.last_name}
+                    </p>
+                    <p>
+                        <strong>Email:</strong>{" "}
+                        {currentUser.user.email}
+                    </p>
+                    <strong>Authorities:</strong>
+                    <ul>
+                        {currentUser.roles &&
+                            currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
+                    </ul>
+                </div>
+            </React.Fragment>
         );
     }
 }
+
+export default Profile
